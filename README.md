4f1e3a7164bd80e3c8d4ed21026327112d210593
Twiddly <pew@pewpew.moe>
2018-07-01 17:19:21 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# plugins
-Plugins for Happypanda X
+
+In this repository resides plugins for HappyPanda X
+
+# How to install
+
+Please see [#Installing plugins](https://happypandax.github.io/usage.html#installing-plugins) in the documentation.
+
+# Be careful about plugins
+
+Read the relevant section [#Be careful about plugins](https://happypandax.github.io/usage.html#be-careful-about-plugins) in documentation
+
+# Plugins
+
+- File Metadata -- Extracts and applies metadata from a file accompanying a gallery. Supports files produced from eze and hdoujin
+
---
+++ build.py
@@ -0,0 +1,49 @@
+import json
+import glob
+import pathlib
+
+readme = """
+In this repository resides plugins for HappyPanda X
+
+# How to install
+
+Please see [#Installing plugins](https://happypandax.github.io/usage.html#installing-plugins) in the documentation.
+
+# Be careful about plugins
+
+Read the relevant section [#Be careful about plugins](https://happypandax.github.io/usage.html#be-careful-about-plugins) in documentation
+
+# Plugins
+
+{}
+"""
+
+plugins_dir = "plugins"
+readme_file = "README.md"
+desc_max_length = 200
+
+def main():
+    print("Building...")
+    plugin_readme = ""
+
+    for p in sorted(glob.glob(f"{plugins_dir}/**/hplugin.json")):
+        with open(p, 'r', encoding="utf-8") as f:
+            d = json.load(f)
+            dir_name = pathlib.Path(p).parent.name
+            plugin_desc = d.get("description")
+            plugin_ver = d.get("version")
+
+            if plugin_desc and plugin_ver:
+                plugin_desc = plugin_desc.split('\n')[0]
+                if len(plugin_desc) > desc_max_length:
+                    plugin_desc = plugin_desc[:desc_max_length] + '...'
+                plugin_readme += f"- {dir_name} `{plugin_ver}` -- {plugin_desc}\n"
+
+    txt = readme.format(plugin_readme)
+
+    with open(readme_file, 'w', encoding="utf-8") as f:
+        f.write(txt)
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
